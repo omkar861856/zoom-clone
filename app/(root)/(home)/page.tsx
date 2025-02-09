16e837b559ad743f5dad25971a6476e7a512cfbe
Omkar Lolge <omkar861856@gmail.com>
2025-02-10 00:46:14 +0530

    first edit

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,34 @@
+'use client';
+
 import MeetingTypeList from '@/components/MeetingTypeList';
+import { useGetCalls } from '@/hooks/useGetCalls';
+import { useEffect } from 'react';
 
 const Home = () => {
+  const { upcomingCalls } = useGetCalls();
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
+  console.log(typeof now);
+  useEffect(() => {
+    upcomingCalls &&
+      upcomingCalls[0] &&
+      console.log(typeof upcomingCalls[0]?.state?.startsAt);
+  }, [upcomingCalls]);
+
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+  const date = new Intl.DateTimeFormat('en-US', { dateStyle: 'full' }).format(
+    now,
+  );
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            Upcoming Meeting at: {time}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
